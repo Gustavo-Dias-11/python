21d0df7f2531e353e859dc31a41a256055cd03c2
Gustavo-Dias-11 <gustavo.dias.raymundi@gmail.com>
2025-03-29 08:18:25 -0300

    Initial commit

---
+++ ex03.py
@@ -0,0 +1,10 @@
+n1 = (input('digite um numero: '))
+n2 = (input('digite outro numero: '))
+s = n1+n2
+print('a soma entre {} e {} é de {}: '.format (n1,n2,s))
+print(n1.isalnum())
+
+# int = numero nao quebrado
+# float = numero quebrado
+# bool = so aceita True e False
+# str = mensagem
---
+++ ex04.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('digite outro valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('A soma é {} o produto é {} e a divisao é {}'.format(s,m,d))
+print('resto da divsao {} e potencia {}'.format (d,di,e))
