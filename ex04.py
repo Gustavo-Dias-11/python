666c4a961471b2d8b0be9c70f8ae16d3405c5567
Gustavo-Dias-11 <gustavo.dias.raymundi@gmail.com>
2025-03-29 08:23:58 -0300

    exerciicio python

--- ex04.py
+++ ex04.py
@@ -5,5 +5,7 @@ m = n1 * n2
 d = n1 / n2
 di = n1 // n2
 e = n1 ** n2
-print('A soma é {} o produto é {} e a divisao é {}'.format(s,m,d))
-print('resto da divsao {} e potencia {}'.format (d,di,e))
+print('A soma é {} o produto é {} e a divisao é {:.3f}'.format(s,m,d))
+print('resto da divsao {} e potencia {}'.format (di,e))
+
+# {:.3f} faz o numero ter 3 casas depois da virgula
